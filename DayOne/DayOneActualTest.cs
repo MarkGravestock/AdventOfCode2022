5768fcd1edb652f1e31e89e26d5e106a3da4f549
MarkGravestock <mark.gravestock@gmail.com>
2022-12-04 21:54:51 +0000

    Calculate Part 2

--- DayOne/DayOneActualTest.cs
+++ DayOne/DayOneActualTest.cs
@@ -5,8 +5,14 @@ namespace DayOne;
 public class DayOneActualTest
 {
     [Fact]
-    public void it_can_find_the_maximum_calories_for_the_real_file()
+    public void it_can_find_the_maximum_calories_of_one_elf_for_the_real_file()
     {
-        new CalorieCounter("input.txt").ElfWithMaximumCalories().Calories.Should().Be(69883);
+        new CalorieCounter("input.txt").ElfWithMaximumCalories().Calories.Should().Be(69_883);
+    }
+
+    [Fact]
+    public void it_can_find_the_total_maximum_calories_for_the_top_3_elves_for_the_real_file()
+    {
+        new CalorieCounter("input.txt").TotalCaloriesOfTopThreeElves().Should().Be(207_576);
     }
 }
